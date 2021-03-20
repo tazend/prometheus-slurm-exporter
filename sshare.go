4bdab8c2fefd7c7afe4a1afde44061529582e8a9
Toni Harzendorf <toni.harzendorf@ufz.de>
2021-03-20 15:31:23 +0100

    Some changes to gathering data from sshare command
      - Can't collect FairShare for accounts when using FAIR_TREE, so ignore for now
      - Collect EffectvUsage and TRESRunMins per account
        - for now, only collect this data for accounts which are direct
          childrens of the root account

--- sshare.go
+++ sshare.go
@@ -24,8 +24,18 @@ import (
         "github.com/prometheus/client_golang/prometheus"
 )
 
+type FairShareMetrics struct {
+	effectv_usage float64
+	alloc_cpu_minutes float64
+	alloc_mem_minutes float64
+}
+
+func FairShareGetMetrics() map[string]*FairShareMetrics {
+	return ParseFairShareMetrics(FairShareData())
+}
+
 func FairShareData() []byte {
-        cmd := exec.Command( "sshare", "-n", "-P", "-o", "account,fairshare" )
+        cmd := exec.Command( "sshare", "-n", "-P", "-o", "Account,EffectvUsage,TRESRunMins" )
         stdout, err := cmd.StdoutPipe()
         if err != nil {
                 log.Fatal(err)
@@ -40,23 +50,40 @@ func FairShareData() []byte {
         return out
 }
 
-type FairShareMetrics struct {
-        fairshare float64
-}
-
-func ParseFairShareMetrics() map[string]*FairShareMetrics {
+func ParseFairShareMetrics(input []byte) map[string]*FairShareMetrics {
         accounts := make(map[string]*FairShareMetrics)
-        lines := strings.Split(string(FairShareData()), "\n")
+
+        lines := strings.Split(string(input), "\n")
         for _, line := range lines {
                 if ! strings.HasPrefix(line,"  ") {
                         if strings.Contains(line,"|") {
+
+				/* Get account name */
                                 account := strings.Trim(strings.Split(line,"|")[0]," ")
+
                                 _,key := accounts[account]
                                 if !key {
-                                        accounts[account] = &FairShareMetrics{0}
+                                        accounts[account] = &FairShareMetrics{0, 0, 0}
                                 }
-                                fairshare,_ := strconv.ParseFloat(strings.Split(line,"|")[1],64)
-                                accounts[account].fairshare = fairshare
+
+				/* Get the effective Usage of the account
+                                 * With the FAIR_TREE algorithm (default), accounts don't have a FairShare value directly,
+                                 * they rather have a LevelFS that helps sorting for usage
+                                 */
+                                effectv_usage,_ := strconv.ParseFloat(strings.Split(line,"|")[1],64)
+
+				/* Parse TRESRunMins (Only CPU and Memory minutes) */
+				tres_run_mins := strings.Trim(strings.Split(line,"|")[2]," ")
+
+				cpu_tres := strings.Trim(strings.Split(tres_run_mins,",")[0]," ")
+				mem_tres := strings.Trim(strings.Split(tres_run_mins,",")[1]," ")
+
+				cpu_minutes,_ := strconv.ParseFloat(strings.Split(cpu_tres, "=")[1], 64)
+				mem_minutes,_ := strconv.ParseFloat(strings.Split(mem_tres, "=")[1], 64)
+
+				accounts[account].alloc_cpu_minutes = cpu_minutes
+				accounts[account].alloc_mem_minutes = mem_minutes
+				accounts[account].effectv_usage = effectv_usage
                         }
                 }
         }
@@ -64,23 +91,31 @@ func ParseFairShareMetrics() map[string]*FairShareMetrics {
 }
 
 type FairShareCollector struct {
-        fairshare *prometheus.Desc
+        effectv_usage     *prometheus.Desc
+        alloc_cpu_minutes *prometheus.Desc
+        alloc_mem_minutes *prometheus.Desc
 }
 
 func NewFairShareCollector() *FairShareCollector {
         labels := []string{"account"}
         return &FairShareCollector{
-                fairshare: prometheus.NewDesc("slurm_account_fairshare","FairShare for account" , labels,nil),
+                effectv_usage: prometheus.NewDesc("slurm_account_effectv_usage", "EffectiveUsage for Account " , labels, nil),
+		alloc_cpu_minutes: prometheus.NewDesc("slurm_account_cpu_minutes", "Allocated CPU-Minutes for Account" , labels, nil),
+		alloc_mem_minutes: prometheus.NewDesc("slurm_account_mem_minutes", "Allocated MEM-Minutes for Account" , labels, nil),
         }
 }
 
 func (fsc *FairShareCollector) Describe(ch chan<- *prometheus.Desc) {
-        ch <- fsc.fairshare
+        ch <- fsc.effectv_usage
+	ch <- fsc.alloc_cpu_minutes
+	ch <- fsc.alloc_mem_minutes
 }
 
 func (fsc *FairShareCollector) Collect(ch chan<- prometheus.Metric) {
-        fsm := ParseFairShareMetrics()
+        fsm := FairShareGetMetrics()
         for f := range fsm {
-                ch <- prometheus.MustNewConstMetric(fsc.fairshare, prometheus.GaugeValue, fsm[f].fairshare, f)
+                ch <- prometheus.MustNewConstMetric(fsc.effectv_usage, prometheus.GaugeValue, fsm[f].effectv_usage, f)
+                ch <- prometheus.MustNewConstMetric(fsc.alloc_cpu_minutes, prometheus.GaugeValue, fsm[f].alloc_cpu_minutes, f)
+                ch <- prometheus.MustNewConstMetric(fsc.alloc_mem_minutes, prometheus.GaugeValue, fsm[f].alloc_mem_minutes, f)
         }
 }
